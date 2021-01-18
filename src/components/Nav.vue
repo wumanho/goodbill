4a27b8ab5b841e735fe4293776317e6177c457fa
wumanho <wumanho@tutanota.com>
2021-01-18 19:36:09 +0800

    以svg的形式添加导入icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="nav">
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/bill">记账</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      标签
+    </router-link>
+    <router-link to="/bill">
+      <svg>
+        <use xlink:href="#bill"/>
+      </svg>
+      记账
+    </router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Nav"
-}
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+    try {
+      importAll(require.context('../assets/icons', true, /\.svg$/));
+    } catch (e) {
+      console.log(e);
+    }
+    export default {
+      name: 'Nav'
+    };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    display: flex;
-    justify-content: space-around;
-  }
+.nav {
+  display: flex;
+  //justify-content: space-around;
+}
 </style>
